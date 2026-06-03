276ca5c913cf8ab75580b0890b46b103d739c690
rahulrp17 <rahulrp4021@gmail.com>
2026-06-03 17:32:49 +0530

    Comment out multiple component imports and usages

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 // import Navbar from "./Components/Navbar";
-import Hero from "./Components/Hero";
-import About from "./Components/About";
-import Skills from "./Components/Skills";
-import Projects from "./Components/Projects";
+// import Hero from "./Components/Hero";
+// import About from "./Components/About";
+// import Skills from "./Components/Skills";
+// import Projects from "./Components/Projects";
 // import Contact from "./Components/Contact";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import Footer from "./Components/Footer";
-import ScrollProgress from "./Components/ScrollProgress";
+// import { ToastContainer } from "react-toastify";
+// import "react-toastify/dist/ReactToastify.css";
+// import Footer from "./Components/Footer";
+// import ScrollProgress from "./Components/ScrollProgress";
 // import Stats from "./Components/Stats";
-import Timeline from "./Components/Timeline";
+// import Timeline from "./Components/Timeline";
 // import Cursor from "./Components/Cursor";
-import CTA from "./Components/CTA";
+// import CTA from "./Components/CTA";
 import { useEffect, useState } from "react";
 import Loader from "./Components/Loader";
-import SocialSidebar from "./Components/SocialSideBar";
+// import SocialSidebar from "./Components/SocialSideBar";
 // import BackToTop from "./Components/BackToTop";
 
 
@@ -34,7 +34,7 @@ function App() {
   }
   return (
     <div className="bg-[#0a0a0a] text-white">
-      <ScrollProgress />
+      {/* <ScrollProgress />
       {/* <Cursor /> */}
       {/* <Navbar /> */}
       <SocialSidebar />
@@ -48,7 +48,7 @@ function App() {
       <CTA />
       {/* <Contact /> */}
       <ToastContainer position="top-right" autoClose={3000} />
-      <Footer />
+      <Footer /> */}
     </div>
   );
 }
